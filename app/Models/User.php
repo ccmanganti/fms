726d52f3d59401f4c0a9080167cbf833bac140a8
ccmanganti <cpe.christopherclarkcmanganti@gmail.com>
2023-08-19 18:04:57 +0800

    Reverted default login and removed profile update

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use JeffGreco13\FilamentBreezy\Traits\TwoFactorAuthenticatable;
+use \Webbingbrasil\FilamentTwoFactor\TwoFactorAuthenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
