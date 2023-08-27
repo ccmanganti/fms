5ca029e8b5899f499d2e9c444b2359a27fac482d
ccmanganti <cpe.christopherclarkcmanganti@gmail.com>
2023-08-27 11:32:44 +0800

    Adjusting User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,6 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-// use \Webbingbrasil\FilamentTwoFactor\TwoFactorAuthenticatable;
 use Jeffgreco13\FilamentBreezy\Traits\TwoFactorAuthenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
